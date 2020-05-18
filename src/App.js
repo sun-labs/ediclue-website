3867ef94ad6344f050d0429d061f0914bfcd2914
Victor Ingman <viregistermail@gmail.com>
2020-05-18 14:38:40 +0200

    animation and styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import logo from './images/logo.png'
 import theme from './theme.json'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+
+import './transitions.css'
 
 const AppWrapper = styled.div`
   min-height: 100%;
@@ -18,15 +21,33 @@ const LogoWrapper = styled.header`
 `
 const MaxWidth = styled.div`
   max-width: ${props => props.maxWidth || 1024}px;
+  position: relative;
+`
+
+const SideBar = styled.nav`
+  position: absolute;
 `
 
 function App () {
   return (
     <AppWrapper>
       <MaxWidth maxWidth={1280}>
-        <LogoWrapper>
-          <img src={logo} />
-        </LogoWrapper>
+        <SideBar>
+          <ul>
+            <li><a href=''>source code</a></li>
+          </ul>
+        </SideBar>
+        <TransitionGroup appear>
+          <CSSTransition
+            classNames='boing'
+            key='logo'
+            timeout={250}
+          >
+            <LogoWrapper>
+              <img src={logo} />
+            </LogoWrapper>
+          </CSSTransition>
+        </TransitionGroup>
       </MaxWidth>
     </AppWrapper>
   )
